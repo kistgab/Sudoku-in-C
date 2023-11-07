2b264df2423465473befc0fbc75566807873a340
gabrielkist <gabrielkist02@gmail.com>
2023-11-06 21:26:58 -0300

    feat: fill board with fixed numbers

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Sudoku
+
 Implementar o Sudoku. Os números pretos são fixos, inicializados no Programa, e os números vermelhos são
 fornecidos pelo usuário. O programa deverá receber as informações e verificar se o Sudoku está correto, ou
 não. Estude as regras do Sudoku.
+
+# Documentação (Regra imposta pela professora)
+
+Fazer comentários para que os colegas que pegarão o código na próxima semana entendam o que foi feito
--- src/main.c
+++ src/main.c
@@ -1,8 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <stdbool.h>
+
+#define TAMANHO_TABULEIRO 9
+
+typedef struct jogada
+{
+  int linha;
+  int coluna;
+  int valor;
+} Jogada;
+
+void preencherTabuleiroComZeros(int tabuleiro[][TAMANHO_TABULEIRO])
+{
+  for (int i = 0; i < TAMANHO_TABULEIRO; i++)
+  {
+    for (int j = 0; j < TAMANHO_TABULEIRO; j++)
+    {
+      tabuleiro[i][j] = 0;
+    }
+  }
+}
+
+void preencherNumerosFixosTabuleiro(int tabuleiro[][TAMANHO_TABULEIRO])
+{
+  // Primeiro quadrante
+  tabuleiro[0][0] = 5;
+  tabuleiro[0][1] = 3;
+  tabuleiro[1][0] = 6;
+  tabuleiro[2][1] = 9;
+  tabuleiro[2][2] = 8;
+
+  // Segundo quadrante
+  tabuleiro[0][4] = 7;
+  tabuleiro[1][3] = 1;
+  tabuleiro[1][4] = 9;
+  tabuleiro[1][5] = 5;
+
+  // Terceiro quadrante
+  tabuleiro[2][7] = 6;
+
+  // Quarto quadrante
+  tabuleiro[3][0] = 8;
+  tabuleiro[4][0] = 4;
+  tabuleiro[5][0] = 7;
+
+  // Quinto quadrante
+  tabuleiro[3][4] = 6;
+  tabuleiro[4][3] = 8;
+  tabuleiro[4][5] = 3;
+  tabuleiro[5][4] = 2;
+
+  // Sexto quadrante
+  tabuleiro[3][8] = 3;
+  tabuleiro[4][8] = 1;
+  tabuleiro[5][8] = 6;
+
+  // Sétimo quadrante
+  tabuleiro[6][1] = 6;
+
+  // Oitavo quadrante
+  tabuleiro[7][3] = 4;
+  tabuleiro[7][4] = 1;
+  tabuleiro[7][5] = 9;
+  tabuleiro[8][4] = 8;
+
+  // Nono quadrante
+  tabuleiro[6][6] = 2;
+  tabuleiro[6][7] = 8;
+  tabuleiro[7][8] = 5;
+  tabuleiro[8][7] = 7;
+  tabuleiro[8][8] = 9;
+}
+
+void imprimirTabuleiro(int tabuleiro[][TAMANHO_TABULEIRO])
+{
+  printf("Colunas ");
+  for (int i = 0; i < TAMANHO_TABULEIRO; i++)
+  {
+    printf("| %d ", i + 1);
+  }
+  printf("|\n\n");
+
+  for (int i = 0; i < TAMANHO_TABULEIRO; i++)
+  {
+    printf("Ln. %d\t", i + 1);
+    for (int j = 0; j < TAMANHO_TABULEIRO; j++)
+    {
+      if (tabuleiro[i][j] > 0 && tabuleiro[i][j] <= 9)
+      {
+        printf("| %d ", tabuleiro[i][j]);
+        continue;
+      }
+      printf("| - ");
+    }
+    printf("|\n");
+  }
+}
 
 int main()
 {
+  int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
   setlocale(LC_ALL, "Portuguese");
+
+  preencherTabuleiroComZeros(tabuleiro);
+  preencherNumerosFixosTabuleiro(tabuleiro);
+  imprimirTabuleiro(tabuleiro);
 }
